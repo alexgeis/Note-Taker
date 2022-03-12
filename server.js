dec2d4751e98435e2ed50574d7f9a629c3fed7cc
Alex Geis <siegxela@gmail.com>
2022-03-11 17:18:43 -0700

    updated server.js - app.list now points to heroku port

--- server.js
+++ server.js
@@ -107,6 +107,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.listen(PORT, () =>
+app.listen(process.env.PORT || PORT, () =>
   console.log(`Express server listening on port ${PORT}!`)
 );
